dcb5597973a8fcd9929b95dd78cb4212078de7c5
Janssenhidal <janssen_schembri@hotmail.com>
2021-07-25 14:59:02 +0200

    Added Header Vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Header />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap");
+* {
+  box-sizing: border-box;
+  font-family: "poppins";
+  font-size: 18px;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <header>
+    <div class="logo-text">
+      <a href="#">
+        <img src="../assets/images/logo.svg" alt="" />
+      </a>
+      <ul>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Resources</a></li>
+      </ul>
+    </div>
+    <ul>
+      <li><a href="#">Login</a></li>
+      <li><a class="signup" href="#">Sign Up</a></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 2rem;
+}
+ul {
+  display: flex;
+  list-style: none;
+}
+li {
+  padding-left: 2rem;
+}
+a {
+  text-decoration: none;
+  color: hsl(257, 7%, 63%);
+  font-size: 0.9rem;
+
+  &:hover {
+    color: hsl(255, 11%, 22%);
+  }
+}
+.logo-text {
+  display: flex;
+}
+
+.signup {
+  background-color: hsl(180, 66%, 49%);
+  padding: 0.5rem 1.25rem;
+  border-radius: 1.5rem;
+  color: #fff;
+  &:hover {
+    opacity: 0.6;
+    color: #fff;
+  }
+}
+</style>
